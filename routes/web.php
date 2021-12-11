3502e4436199307faff175dffef19079f0038180
Sharupya <sharupyabarua1427@gmail.com>
2021-12-11 14:22:11 +0600

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
+Route::get('/dashboard', [AuthController::class, 'dashboard']);
