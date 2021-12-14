bf46be209f1c7d20ee8ea94e145043d1846a955c
SobuzDas <sobujthegreat@gmail.com>
2021-12-14 20:08:48 +0600

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,13 @@ Route::get('/', function () {
 
 Route::get('/login', [AdminController::class, 'login']);
 Route::post('/loginstore', [AdminController::class, 'loginstore']);
-<<<<<<< HEAD
 Route::get('/admindash', [AdminController::class, 'admindash']);
 Route::get('/plogin', [PaitentController::class, 'plogin']);
 Route::get('/registration', [PaitentController::class,'registration']);
 Route::post('/store', [PaitentController::class,'store']);
 Route::post('/store', [LoginController::class,'store']);
-=======
-Route::get('/dashboard', [AdminController::class, 'dashboard']);
-Route::get('/registration',[PatientController::class,'registration']);
+
 Route::get('pdashboard', function(){
     return view('patient.layouts.pdashboard');
 });
->>>>>>> 33b85e0fce6244345336746f87848d6027bcadab
+
